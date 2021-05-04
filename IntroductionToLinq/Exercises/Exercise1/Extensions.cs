bfad88802011d325ca5f14d86dcff0babaeed4e5
RinatBlinik <82043211+RinatBlinik@users.noreply.github.com>
2021-05-04 20:18:04 +0300

    initial commit

---
+++ IntroductionToLinq/Exercises/Exercise1/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroductionToLinq.Exercise1
+{
+    public static class ExtensionsSolution
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector)
+        {
+            foreach (var item in source)
+            {
+                yield return selector(item);
+            }
+        }
+
+        public static IEnumerable<TResult> MySelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (var item in source)
+            {
+                foreach (var t in selector(item))
+                {
+                    yield return t;
+                }
+            }
+        }
+
+        public static int MySum(this IEnumerable<int> source)
+        {
+            int sum = 0;
+            foreach (var item in source)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public static int MyCount<TSource>(this IEnumerable<TSource> source)
+        {
+            int count = 0;
+            foreach (var item in source)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ IntroductionToLinq/Exercises/Exercise2/ExerciseTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntroductionToLinq.Exercise2
+{
+    // Perform this exercises after studying 01-09
+
+    [TestClass]
+    public class ExerciseTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            // Find the name of the company with the largest number of orders that
+            // is located in the city of "Buenos Aires" 
+
+            // This is incorrect. Replace the right side with the correct query
+
+            // var companyName = Customers.CustomerList.Last().CompanyName;
+            var companyName = Customers.CustomerList.Where(c => c.City == "Buenos Aires").Aggregate((currentMax, nextCustomer) => currentMax.Orders.Length < nextCustomer.Orders.Length ? nextCustomer : currentMax).CompanyName;
+            Assert.AreEqual("Cactus Comidas para llevar", companyName);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            List<Customer> customers = Customers.CustomerList;
+
+            // Representing an order as a tuple (CustomerID, OrderID, Total)
+            // find all the orders whose Total is smaller than 20M
+
+            // This is incorrect. Replace the right side with the correct query
+
+            var  orders = customers.SelectMany(c => c.Orders, (c, o) => (c.CustomerID, o.OrderID, o.Total)).Where(triple => triple.Total < 20M);
+            var expected = new[]
+            {
+                ("CACTU", 10782, 12.50M),
+                ("FRANS", 10807, 18.40M)
+            };
+
+            Assert.IsTrue(expected.SequenceEqual(orders));
+
+        }
+    }
+}
